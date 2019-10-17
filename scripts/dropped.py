771af07923382aaa37aa5f069d97bae68ba81992
piyushp648 <piyushparwani@outlook.com>
2019-10-17 15:36:04 +0530

    Corrected quote marks

--- scripts/dropped.py
+++ scripts/dropped.py
@@ -12,10 +12,12 @@ with open(INPUT_FILE, encoding="utf8") as f:
 #         print(company['name'] + str(exp['votes']))
 
 with open(OUTPUT_FILE, mode='w',encoding="utf8",newline='') as exp_file:
-    exp_writer = csv.writer(exp_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    exp_writer = csv.writer(exp_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
 
     exp_writer.writerow(['Company', 'Experience'])
     for company in data:
         for exp in company['experiences']:
             # print(company['name'] + str(exp['votes']))
-            exp_writer.writerow([company['name'], exp['text']])
+            if not exp['text']: continue
+            exp_writer.writerow([company['name'], str(exp['text'])])
+
