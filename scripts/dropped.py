77160d117ae9bc11987fa8c2472372c28a427c62
piyushp648 <piyushparwani@outlook.com>
2019-10-20 20:52:29 +0530

    replaced unicode character \xa0 with ascii whitespace

--- scripts/dropped.py
+++ scripts/dropped.py
@@ -1,17 +1,18 @@
 import json
 import csv
+import io
 ## TF-IDF 
 INPUT_FILE = 'text_stripped.json'
 OUTPUT_FILE = 'exp.csv'
 
-with open(INPUT_FILE, encoding="utf8") as f:
+with io.open(INPUT_FILE, encoding="utf8") as f:
     data = json.load(f)
 
 # for company in data:
 #     for exp in company['experiences']:
 #         print(company['name'] + str(exp['votes']))
 
-with open(OUTPUT_FILE, mode='w',encoding="utf8",newline='') as exp_file:
+with io.open(OUTPUT_FILE, mode='w',encoding="utf8",newline='') as exp_file:
     exp_writer = csv.writer(exp_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
 
     exp_writer.writerow(['Company', 'Experience'])
--- scripts/strip_n.py
+++ scripts/strip_n.py
@@ -1,16 +1,19 @@
 import json
 from tqdm import tqdm
-## TF-IDF 
-INPUT_FILE = 'output/companies_with_exp_text.json'
-OUTPUT_FILE = 'output/text_stripped.json'
+import io
 
-with open(INPUT_FILE, encoding="utf-8") as f:
+## TF-IDF 
+INPUT_FILE = 'companies_with_exp_text.json'
+OUTPUT_FILE = 'text_stripped.json'
+
+with io.open(INPUT_FILE, encoding="utf-8") as f:
     data = json.load(f)
 
 for company in tqdm(data, desc='Total'):
     for exp in tqdm(company['experiences'], desc=company['name']):
         exp['text'] = exp['text'].replace("\n", " ")
+        exp['text'] = exp['text'].replace(u'\xa0', u' ')
 
 print("\n\n")
-with open(OUTPUT_FILE, "w") as file:
+with io.open(OUTPUT_FILE, "w", encoding='utf8') as file:
     json.dump(data, file, ensure_ascii=False, indent=4)
